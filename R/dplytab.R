c20ae2d7a20a4c58bf549f3dd89d5a107961d4d7
danmsf <danmsf@gmail.com>
2017-06-25 22:34:45 +0300

    Got rid of default text

--- R/dplytab.R
+++ R/dplytab.R
@@ -1,25 +1,8 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-# require(dplyr)
-# require(tidyr)
+# Hello, dplytab!
 
-#
-#
-# library(lazyeval)
-
+require(dplyr)
 require(tidyr)
+
 
 dplytab <- function(data, row_by , col_by , evalvars=NA) {
   varnames <- names(data)
@@ -32,24 +15,3 @@ dplytab <- function(data, row_by , col_by , evalvars=NA) {
     select(.,c(row_by, names(.)) ) %>% arrange_(row_by)
 }
 
-
-
-irist <- iris
-irist <- cbind(irist, dum = c(rep(0,75),rep(1,75)),dum2 = c(rep("a",45),rep("b",105)))
-
-stat <- irist %>%
-  group_by(Species, dum, dum2) %>%
-  summarise_at(vars(Sepal.Length), funs(sd))
-
-irist %>%
-  group_by(Species, dum, dum2) %>%
-  summarise_at(vars(Sepal.Length), funs(n(),sd, mean)) %>%
-  dplytab(row_by=c("dum2", "Species")
-          ,col_by = c("dum"))
-
-
-
-irist %>%
-  group_by(Species, dum, dum2) %>%
-  summarise_at(vars(Sepal.Length), funs(n(),sd, mean)) %>%
-  dplytab(row_by=c("dum2", "Species") )
